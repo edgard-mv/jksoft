a5155b86f67b85c38eab07516ef6cafafae76184
edgard-mv <edgardmv04@gmail.com>
2020-04-26 21:34:47 -0600

    comentarios modificados en menu.blade.php

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -5,10 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <!-- Bootstrap core -->
     <script src="js/app.js"></script>
     <link rel="stylesheet" href="css/app.css">
-      <!-- Bootstrap core CSS -->
-    <!--<link href="css/bootstrap.min.css" rel="stylesheet">-->
 
       <!-- Custom styles for this template -->
     <link href="css/simple-sidebar.css" rel="stylesheet">
