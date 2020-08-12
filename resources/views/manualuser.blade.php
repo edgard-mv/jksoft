b17c6bd38643be07582f28fa5d2ecbbe3313bf42
katedesbas <valesoza001@gmail.com>
2020-08-12 12:49:00 -0600

    Vistas y Backup

--- resources/views/manualuser.blade.php
+++ resources/views/manualuser.blade.php
@@ -2,7 +2,7 @@
 @section('titulo',"Manual de usuario")
 @section('content')
 
-<iframe src="https://docs.google.com/viewer?srcid=1PX9KBwufou6pCIYGanQc9KWR8JppaKm7&pid=explorer&efh=false&a=v&chrome=false&embedded=true"
+<iframe src="https://docs.google.com/viewer?srcid=1_OsxwQljYRPSQ-kVATi_EvhhbF104jN8&pid=explorer&efh=false&a=v&chrome=false&embedded=true"
  width="100%" height="480px"></iframe>
 
 @endsection
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -86,10 +86,7 @@
     </div>
    
 
- 
-  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-  <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
-  <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+
 
 </body>
 </html>
